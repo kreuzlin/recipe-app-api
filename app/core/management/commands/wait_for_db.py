8b61d9a683077495075f82cee156e07abd7dde0b
Linard Paly <linard.paly@me.com>
2022-11-14 07:23:07 +0100

    error correction

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,11 +1,11 @@
 """
-Django commant to wait for the database to be available.
+Django command to wait for the database to be available.
 """
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-        """Django command to wait for database."""
+    """Django command to wait for database."""
 
-        def handle(self, *args, **options):
-            pass
+    def handle(self, *args, **options):
+        pass
