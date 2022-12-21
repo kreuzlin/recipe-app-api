6c46f69cde8fd1741131ef5e37acfaf64c81aa2e
Linard Paly <linard.paly@me.com>
2022-12-21 06:20:47 +0100

    linting errors

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -46,3 +46,4 @@ class AuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+        
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -108,3 +108,4 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
